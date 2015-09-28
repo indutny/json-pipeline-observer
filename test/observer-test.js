d21acd57cc6eab29c5148e475212e1c26ceee097
Fedor Indutny <fedor@indutny.com>
2015-09-28 15:27:22 -0400

    initial

---
+++ test/observer-test.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var assert = require('assert');
+var pipeline = require('json-pipeline');
+
+var observer = require('../');
+
+describe('Observer', function() {
+  var p;
+  var o;
+  beforeEach(function() {
+    p = pipeline.create();
+    o = observer.create(p);
+  });
+
+  it('should observe addition of nodes/inputs', function(done) {
+    var node = p.create('node', p.create('first-input'));
+    node.addInput(p.create('second-input'));
+
+    setImmediate(function() {
+      assert.deepEqual(o.log, [
+        { action: 'create', node: 0, opcode: 'first-input' },
+        { action: 'create', node: 1, opcode: 'node' },
+        { action: 'addInput', node: 1, other: 0 },
+        { action: 'create', node: 2, opcode: 'second-input' },
+        { action: 'addInput', node: 1, other: 2 }
+      ])
+      done();
+    });
+  });
+
+  it('should observe removal of inputs', function(done) {
+    var node = p.create('node', [
+      p.create('first-input'),
+      p.create('second-input') ]);
+    node.clearInputs();
+
+    setImmediate(function() {
+      assert.deepEqual(o.log, [
+        { action: 'create', node: 0, opcode: 'first-input' },
+        { action: 'create', node: 1, opcode: 'second-input' },
+        { action: 'create', node: 2, opcode: 'node' },
+        { action: 'addInput', node: 2, other: 0 },
+        { action: 'addInput', node: 2, other: 1 },
+        { action: 'removeInput', node: 2, old: 1, index: 1 },
+        { action: 'removeInput', node: 2, old: 0, index: 0 }
+      ])
+      done();
+    });
+  });
+
+  it('should observe replacement of inputs', function(done) {
+    var node = p.create('node', [
+      p.create('first-input'),
+      p.create('second-input') ]);
+    node.replaceInput(0, p.create('replacement'));
+
+    setImmediate(function() {
+      assert.deepEqual(o.log, [
+        { action: 'create', node: 0, opcode: 'first-input' },
+        { action: 'create', node: 1, opcode: 'second-input' },
+        { action: 'create', node: 2, opcode: 'node' },
+        { action: 'addInput', node: 2, other: 0 },
+        { action: 'addInput', node: 2, other: 1 },
+        { action: 'create', node: 3, opcode: 'replacement' },
+        { action: 'replaceInput', node: 2, index: 0, from: 0, to: 3 }
+      ])
+      done();
+    });
+  });
+
+  it('should observe change of opcode', function(done) {
+    var node = p.create('node');
+    node.opcode = 'new-node';
+
+    setImmediate(function() {
+      assert.deepEqual(o.log, [
+        { action: 'create', node: 0, opcode: 'node' },
+        { action: 'changeOpcode', node: 0, from: 'node', to: 'new-node' }
+      ])
+      done();
+    });
+  });
+
+  it('should observe change of index', function(done) {
+    var node = p.create('node');
+    var other = p.create('other-node');
+    p.remove(node);
+
+    setImmediate(function() {
+      assert.deepEqual(o.log, [
+        { action: 'create', node: 0, opcode: 'node' },
+        { action: 'create', node: 1, opcode: 'other-node' },
+        { action: 'changeIndex', node: 0, from: 0, to: -1 },
+        { action: 'changeIndex', node: 1, from: 1, to: 0 }
+      ])
+      done();
+    });
+  });
+});
